64e84ca74ce228aa510efcc2e63ca1b69a16eed7
윤기태 <yoon@yungitaeui-MacBook-Pro.local>
2017-04-29 02:14:56 +0900

    image upload and redireciton

--- board/views.py
+++ board/views.py
@@ -7,15 +7,24 @@ import tensorflow as tf
 
 from django.views.decorators.csrf import csrf_exempt
 from django.http import JsonResponse
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, render_to_response
 from django.http import HttpResponseRedirect
 
 from processor.models import Image
 
 
+@csrf_exempt
 def Board(request):
     #list = Image.objects.all()
     hello = tf.constant('Hello, TensorFlow!')
     sess = tf.Session()
     tensor = str(sess.run(hello))
     return render(request, 'pages/board/board.html', {'tensor': tensor})
+
+
+@csrf_exempt
+def GetImage(request):
+    files = request.FILES.getlist('file')
+    print (files)
+
+    return HttpResponseRedirect('/board')
